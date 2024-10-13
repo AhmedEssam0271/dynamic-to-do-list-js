3672b5bc955ab135dfc69fd8a89dc1ebd71e44aa
AhmedEssam0271 <ahmedessam0271@gmail.com>
2024-10-14 00:43:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-document.addEventListener("DOMContentLoaded", (event) => {});
 const addButton = document.getElementById("add-task-btn");
 const taskInput = document.getElementById("task-input");
 const taskList = document.getElementById("task-list");
 
-function addTask() {
+function loadTasks() {
+  const storedTasks = JSON.parse(localStorage.getItem("tasks") || "[]");
+  storedTasks.forEach((taskText) => addTask(taskText, false)); // 'false' indicates not to save again to Local Storage
+}
+
+function addTask(taskText, save = true) {
   let taskText = taskInput.value.trim();
   if (taskText == "") {
     alert("Please enter a task.");
@@ -13,6 +17,8 @@ function addTask() {
     const node = document.createTextNode(taskText);
     li.appendChild(node);
     taskList.appendChild(li);
+    let tasks = taskText;
+    localStorage.setItem("tasks", taskText);
     const removeButton = document
       .createElement("button")
       .classList.add("remove-btn");
@@ -21,8 +27,13 @@ function addTask() {
     taskList.appendChild(li);
     taskList.value = "";
   }
+  if (save) {
+    const storedTasks = JSON.parse(localStorage.getItem("tasks") || "[]");
+    storedTasks.push(taskText);
+    localStorage.setItem("tasks", JSON.stringify(storedTasks));
+  }
 }
-document.addEventListener("DOMContentLoaded", addTask);
+document.addEventListener("DOMContentLoaded", loadTasks, addTask);
 addButton.addEventListener("click", addTask);
 taskInput.addEventListener("keypress", (validate) => {
   if (event.key === "Enter") {
