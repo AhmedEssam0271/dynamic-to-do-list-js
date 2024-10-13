2bce123b83a85f48ad13863b7391bd3a7c5a9880
AhmedEssam0271 <ahmedessam0271@gmail.com>
2024-10-14 01:14:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,8 @@ const addButton = document.getElementById("add-task-btn");
 const taskInput = document.getElementById("task-input");
 const taskList = document.getElementById("task-list");
 
-function loadTasks() {
-  const storedTasks = JSON.parse(localStorage.getItem("tasks") || "[]");
-  storedTasks.forEach((taskText) => addTask(taskText, false)); // 'false' indicates not to save again to Local Storage
-}
-
 function addTask(taskText, save = true) {
-  let taskText = taskInput.value.trim();
+  taskText = taskInput.value.trim();
   if (taskText == "") {
     alert("Please enter a task.");
   }
@@ -19,10 +14,13 @@ function addTask(taskText, save = true) {
     taskList.appendChild(li);
     let tasks = taskText;
     localStorage.setItem("tasks", taskText);
-    const removeButton = document
-      .createElement("button")
-      .classList.add("remove-btn");
-    removeButton.onclick(taskList.Remove(li));
+    let removeButton = document.createElement("button");
+    removeButton.classList.add("remove-btn");
+    removeButton.innerHTML = "Remove";
+    function removeLi() {
+      li.remove();
+    }
+    removeButton.addEventListener("click", removeLi);
     li.appendChild(removeButton);
     taskList.appendChild(li);
     taskList.value = "";
@@ -33,6 +31,11 @@ function addTask(taskText, save = true) {
     localStorage.setItem("tasks", JSON.stringify(storedTasks));
   }
 }
+function loadTasks() {
+  const storedTasks = JSON.parse(localStorage.getItem("tasks") || "[]");
+  storedTasks.forEach((taskText) => addTask(taskText, false)); // 'false' indicates not to save again to Local Storage
+}
+
 document.addEventListener("DOMContentLoaded", loadTasks, addTask);
 addButton.addEventListener("click", addTask);
 taskInput.addEventListener("keypress", (validate) => {
